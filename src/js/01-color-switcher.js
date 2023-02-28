2ea04773a7cd3a9f527e7dbec124ec08de0a8417
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-02-28 19:08:35 +0000

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+const switcher = {
+  isActive: false,
+  switchId: null,
+
+  onStartButtonClick() {
+    if (this.isActive) return;
+
+    this.isActive = true;
+
+    this.switchId = setInterval(() => {
+      refs.btnStart.disabled = true;
+      refs.btnStop.disabled = false;
+      document.body.style.backgroundColor = this.getRandomHexColor();
+    }, 1000);
+  },
+  onStopButtonClick() {
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+    clearInterval(this.switchId);
+    this.switchId = undefined;
+  },
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+};
+
+refs.btnStart.addEventListener('click', () => {
+  switcher.onStartButtonClick();
+});
+refs.btnStop.addEventListener('click', () => {
+  switcher.onStopButtonClick();
+});
