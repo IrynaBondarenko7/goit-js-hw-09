520e069ccffe8b0cce9a2d62346529c7f8b0107a
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-03-05 13:13:30 +0000

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+//функція, яка створює і повертає один проміс
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  //доступ до значень елементів форми
+  const {
+    elements: { delay, step, amount },
+  } = evt.currentTarget;
+
+  let firstDelay = Number(delay.value);
+  const stepDeley = Number(step.value);
+
+  //цикл створює стільки промісів, скільки введено в поле amount
+  for (let i = 0; i < amount.value; i++) {
+    createPromise(1 + i, firstDelay + i * stepDeley)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
